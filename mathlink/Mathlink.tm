f7c8c8f3a4dfc2666c21bf76e2c4bc0d3dc71987
Carlos Manuel <fis.carlosmanuel@gmail.com>
2022-02-15 17:16:59 -0600

    Begin mathlink implementation

---
+++ mathlink/Mathlink.tm
@@ -0,0 +1,7 @@
+:Begin:
+:Function:       test
+:Pattern:        Test[]
+:Arguments:      Manual
+:ArgumentTypes:  Manual
+:ReturnType:     Manual
+:End:
---
+++ mathlink/MathlinkInterface.cpp
@@ -0,0 +1,43 @@
+#include "mathlink.h"
+
+#if defined(WINDOWS_MATHLINK)
+#include <windows.h>
+#endif
+
+void test()
+{
+
+}
+
+/****************************
+*                           *
+*   Mathlink entry point    *
+*                           *
+****************************/
+
+#if defined(WINDOWS_MATHLINK)
+
+extern HWND MLInitializeIcon(HINSTANCE hinstCurrent, int nCmdShow);
+
+int WINAPI WinMain(HINSTANCE hinstCurrent, HINSTANCE hinstPrevious, LPSTR lpszCmdLine, int nCmdShow)
+{
+    char buff[512];
+    char* buff_start = buff;
+    char* argv[32];
+    char** argv_end = argv + 32;
+
+    hinstPrevious = hinstPrevious; /* suppress warning */
+
+    if (!MLInitializeIcon(hinstCurrent, nCmdShow)) return 1;
+    MLScanString(argv, &argv_end, &lpszCmdLine, &buff_start);
+    return MLMain((int)(argv_end - argv), argv);
+}
+
+#else
+
+int main(int argc, char* argv[])
+{
+    return MLMain(argc, argv);
+}
+
+#endif
