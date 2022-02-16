1095eff2e6712c9491b89ad94f0729dca31c378a
Carlos Manuel <fis.carlosmanuel@gmail.com>
2022-02-16 15:15:31 -0600

    Update TestStatistics.h

--- include/TestStatistics.h
+++ include/TestStatistics.h
@@ -14,8 +14,8 @@ double ks_statistic(const DiscreteEmpiricalDistribution& empirical, const Discre
 double calculate_ks_statistic_of_fit(const std::vector<int>& sampleData);
 double calculate_ks_statistic_of_fit(const DiscretePowerLawDistribution &fittedModel, const std::vector<int>& sampleData);
 
-double calculate_gof(const std::vector<int>& sampleData, int replicas = 1000, RuntimeMode mode = RuntimeMode::SingleThread,
-                     DiscreteRandomSampleType sampleType = DiscreteRandomSampleType::Approximate);
+double calculate_gof(const std::vector<int>& sampleData, int replicas = 1000, RuntimeMode mode = RuntimeMode::MultiThread,
+                     DiscreteRandomSampleType sampleType = DiscreteRandomSampleType::Precise);
 double calculate_gof(const DiscretePowerLawDistribution& fittedModel, const std::vector<int>& sampleData,
-                     int replicas = 1000, RuntimeMode mode = RuntimeMode::SingleThread,
-                     DiscreteRandomSampleType sampleType = DiscreteRandomSampleType::Approximate);
+                     int replicas = 1000, RuntimeMode mode = RuntimeMode::MultiThread,
+                     DiscreteRandomSampleType sampleType = DiscreteRandomSampleType::Precise);
