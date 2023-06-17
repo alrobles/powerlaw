06f86b1b52242d5ed1fafa0e2972eb98dd36907a
alrobles <a.l.robles.fernandez@gmail.com>
2023-06-17 07:40:01 -0700

    change deb library dependence

---
+++ build/CMakeFiles/PowerLawFitterCppApp.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PowerLawFitterCppApp.dir/cli/CLIMain.cpp.o"
+  "CMakeFiles/PowerLawFitterCppApp.dir/cli/CLIMain.cpp.o.d"
+  "CMakeFiles/PowerLawFitterCppApp.dir/src/DiscreteDistributions.cpp.o"
+  "CMakeFiles/PowerLawFitterCppApp.dir/src/DiscreteDistributions.cpp.o.d"
+  "CMakeFiles/PowerLawFitterCppApp.dir/src/ProgressBar.cpp.o"
+  "CMakeFiles/PowerLawFitterCppApp.dir/src/ProgressBar.cpp.o.d"
+  "CMakeFiles/PowerLawFitterCppApp.dir/src/RandomGen.cpp.o"
+  "CMakeFiles/PowerLawFitterCppApp.dir/src/RandomGen.cpp.o.d"
+  "CMakeFiles/PowerLawFitterCppApp.dir/src/TestStatistics.cpp.o"
+  "CMakeFiles/PowerLawFitterCppApp.dir/src/TestStatistics.cpp.o.d"
+  "CMakeFiles/PowerLawFitterCppApp.dir/src/Zeta.cpp.o"
+  "CMakeFiles/PowerLawFitterCppApp.dir/src/Zeta.cpp.o.d"
+  "PowerLawFitterCppApp"
+  "PowerLawFitterCppApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PowerLawFitterCppApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/PowerLawFitterCppApp.dir/src/Zeta.cpp.o.d
@@ -0,0 +1,224 @@
+CMakeFiles/PowerLawFitterCppApp.dir/src/Zeta.cpp.o: \
+ /home/alrobles/powerlaw/src/Zeta.cpp /usr/include/stdc-predef.h \
+ /usr/include/gsl/gsl_sf.h /usr/include/gsl/gsl_sf_result.h \
+ /usr/include/gsl/gsl_sf_airy.h /usr/include/gsl/gsl_mode.h \
+ /usr/include/gsl/gsl_inline.h /usr/include/gsl/gsl_sf_bessel.h \
+ /usr/include/c++/9/stdlib.h /usr/include/c++/9/cstdlib \
+ /usr/include/x86_64-linux-gnu/c++/9/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/9/bits/os_defines.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/9/bits/cpu_defines.h \
+ /usr/include/c++/9/pstl/pstl_config.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/select2.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-bsearch.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib.h \
+ /usr/include/c++/9/bits/std_abs.h /usr/include/gsl/gsl_precision.h \
+ /usr/include/gsl/gsl_types.h /usr/include/gsl/gsl_sf_clausen.h \
+ /usr/include/gsl/gsl_sf_coupling.h /usr/include/gsl/gsl_sf_coulomb.h \
+ /usr/include/gsl/gsl_sf_dawson.h /usr/include/gsl/gsl_sf_debye.h \
+ /usr/include/gsl/gsl_sf_dilog.h /usr/include/gsl/gsl_sf_elementary.h \
+ /usr/include/gsl/gsl_sf_ellint.h /usr/include/gsl/gsl_sf_elljac.h \
+ /usr/include/gsl/gsl_sf_erf.h /usr/include/gsl/gsl_sf_exp.h \
+ /usr/include/gsl/gsl_sf_expint.h /usr/include/gsl/gsl_sf_fermi_dirac.h \
+ /usr/include/gsl/gsl_sf_gamma.h /usr/include/gsl/gsl_sf_gegenbauer.h \
+ /usr/include/gsl/gsl_sf_hermite.h /usr/include/gsl/gsl_sf_hyperg.h \
+ /usr/include/gsl/gsl_sf_laguerre.h /usr/include/gsl/gsl_sf_lambert.h \
+ /usr/include/gsl/gsl_sf_legendre.h /usr/include/gsl/gsl_sf_log.h \
+ /usr/include/gsl/gsl_sf_mathieu.h /usr/include/gsl/gsl_eigen.h \
+ /usr/include/gsl/gsl_vector.h \
+ /usr/include/gsl/gsl_vector_complex_long_double.h \
+ /usr/include/gsl/gsl_errno.h /usr/include/stdio.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio2.h /usr/include/errno.h \
+ /usr/include/x86_64-linux-gnu/bits/errno.h /usr/include/linux/errno.h \
+ /usr/include/x86_64-linux-gnu/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/x86_64-linux-gnu/bits/types/error_t.h \
+ /usr/include/gsl/gsl_complex.h /usr/include/gsl/gsl_check_range.h \
+ /usr/include/gsl/gsl_vector_long_double.h \
+ /usr/include/gsl/gsl_block_long_double.h \
+ /usr/include/gsl/gsl_vector_complex.h \
+ /usr/include/gsl/gsl_block_complex_long_double.h \
+ /usr/include/gsl/gsl_vector_complex_double.h \
+ /usr/include/gsl/gsl_vector_double.h /usr/include/gsl/gsl_block_double.h \
+ /usr/include/gsl/gsl_block_complex_double.h \
+ /usr/include/gsl/gsl_vector_complex_float.h \
+ /usr/include/gsl/gsl_vector_float.h /usr/include/gsl/gsl_block_float.h \
+ /usr/include/gsl/gsl_block_complex_float.h \
+ /usr/include/gsl/gsl_vector_ulong.h /usr/include/gsl/gsl_block_ulong.h \
+ /usr/include/gsl/gsl_vector_long.h /usr/include/gsl/gsl_block_long.h \
+ /usr/include/gsl/gsl_vector_uint.h /usr/include/gsl/gsl_block_uint.h \
+ /usr/include/gsl/gsl_vector_int.h /usr/include/gsl/gsl_block_int.h \
+ /usr/include/gsl/gsl_vector_ushort.h /usr/include/gsl/gsl_block_ushort.h \
+ /usr/include/gsl/gsl_vector_short.h /usr/include/gsl/gsl_block_short.h \
+ /usr/include/gsl/gsl_vector_uchar.h /usr/include/gsl/gsl_block_uchar.h \
+ /usr/include/gsl/gsl_vector_char.h /usr/include/gsl/gsl_block_char.h \
+ /usr/include/gsl/gsl_matrix.h \
+ /usr/include/gsl/gsl_matrix_complex_long_double.h \
+ /usr/include/gsl/gsl_matrix_complex_double.h \
+ /usr/include/gsl/gsl_matrix_complex_float.h \
+ /usr/include/gsl/gsl_matrix_long_double.h \
+ /usr/include/gsl/gsl_matrix_double.h /usr/include/gsl/gsl_matrix_float.h \
+ /usr/include/gsl/gsl_matrix_ulong.h /usr/include/gsl/gsl_matrix_long.h \
+ /usr/include/gsl/gsl_matrix_uint.h /usr/include/gsl/gsl_matrix_int.h \
+ /usr/include/gsl/gsl_matrix_ushort.h /usr/include/gsl/gsl_matrix_short.h \
+ /usr/include/gsl/gsl_matrix_uchar.h /usr/include/gsl/gsl_matrix_char.h \
+ /usr/include/gsl/gsl_sf_pow_int.h /usr/include/gsl/gsl_sf_psi.h \
+ /usr/include/gsl/gsl_sf_sincos_pi.h \
+ /usr/include/gsl/gsl_sf_synchrotron.h \
+ /usr/include/gsl/gsl_sf_transport.h /usr/include/gsl/gsl_sf_trig.h \
+ /usr/include/gsl/gsl_sf_zeta.h /usr/include/c++/9/complex \
+ /usr/include/c++/9/bits/cpp_type_traits.h \
+ /usr/include/c++/9/ext/type_traits.h /usr/include/c++/9/cmath \
+ /usr/include/math.h /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-narrow.h \
+ /usr/include/x86_64-linux-gnu/bits/iscanonical.h \
+ /usr/include/x86_64-linux-gnu/bits/mathinline.h \
+ /usr/include/c++/9/bits/specfun.h /usr/include/c++/9/bits/stl_algobase.h \
+ /usr/include/c++/9/bits/functexcept.h \
+ /usr/include/c++/9/bits/exception_defines.h \
+ /usr/include/c++/9/ext/numeric_traits.h \
+ /usr/include/c++/9/bits/stl_pair.h /usr/include/c++/9/bits/move.h \
+ /usr/include/c++/9/bits/concept_check.h /usr/include/c++/9/type_traits \
+ /usr/include/c++/9/bits/stl_iterator_base_types.h \
+ /usr/include/c++/9/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/9/debug/assertions.h \
+ /usr/include/c++/9/bits/stl_iterator.h \
+ /usr/include/c++/9/bits/ptr_traits.h /usr/include/c++/9/debug/debug.h \
+ /usr/include/c++/9/bits/predefined_ops.h /usr/include/c++/9/limits \
+ /usr/include/c++/9/tr1/gamma.tcc \
+ /usr/include/c++/9/tr1/special_function_util.h \
+ /usr/include/c++/9/tr1/bessel_function.tcc \
+ /usr/include/c++/9/tr1/beta_function.tcc \
+ /usr/include/c++/9/tr1/ell_integral.tcc \
+ /usr/include/c++/9/tr1/exp_integral.tcc \
+ /usr/include/c++/9/tr1/hypergeometric.tcc \
+ /usr/include/c++/9/tr1/legendre_function.tcc \
+ /usr/include/c++/9/tr1/modified_bessel_func.tcc \
+ /usr/include/c++/9/tr1/poly_hermite.tcc \
+ /usr/include/c++/9/tr1/poly_laguerre.tcc \
+ /usr/include/c++/9/tr1/riemann_zeta.tcc /usr/include/c++/9/sstream \
+ /usr/include/c++/9/istream /usr/include/c++/9/ios \
+ /usr/include/c++/9/iosfwd /usr/include/c++/9/bits/stringfwd.h \
+ /usr/include/c++/9/bits/memoryfwd.h /usr/include/c++/9/bits/postypes.h \
+ /usr/include/c++/9/cwchar /usr/include/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/types/wint_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar2.h /usr/include/c++/9/exception \
+ /usr/include/c++/9/bits/exception.h \
+ /usr/include/c++/9/bits/exception_ptr.h \
+ /usr/include/c++/9/bits/cxxabi_init_exception.h \
+ /usr/include/c++/9/typeinfo /usr/include/c++/9/bits/hash_bytes.h \
+ /usr/include/c++/9/new /usr/include/c++/9/bits/nested_exception.h \
+ /usr/include/c++/9/bits/char_traits.h /usr/include/c++/9/cstdint \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/c++/9/bits/localefwd.h \
+ /usr/include/x86_64-linux-gnu/c++/9/bits/c++locale.h \
+ /usr/include/c++/9/clocale /usr/include/locale.h \
+ /usr/include/x86_64-linux-gnu/bits/locale.h /usr/include/c++/9/cctype \
+ /usr/include/ctype.h /usr/include/c++/9/bits/ios_base.h \
+ /usr/include/c++/9/ext/atomicity.h \
+ /usr/include/x86_64-linux-gnu/c++/9/bits/gthr.h \
+ /usr/include/x86_64-linux-gnu/c++/9/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_sched_param.h \
+ /usr/include/x86_64-linux-gnu/bits/cpu-set.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/bits/timex.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_tm.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_itimerspec.h \
+ /usr/include/x86_64-linux-gnu/bits/setjmp.h \
+ /usr/include/x86_64-linux-gnu/c++/9/bits/atomic_word.h \
+ /usr/include/c++/9/bits/locale_classes.h /usr/include/c++/9/string \
+ /usr/include/c++/9/bits/allocator.h \
+ /usr/include/x86_64-linux-gnu/c++/9/bits/c++allocator.h \
+ /usr/include/c++/9/ext/new_allocator.h \
+ /usr/include/c++/9/bits/ostream_insert.h \
+ /usr/include/c++/9/bits/cxxabi_forced.h \
+ /usr/include/c++/9/bits/stl_function.h \
+ /usr/include/c++/9/backward/binders.h /usr/include/c++/9/bits/stl_algo.h \
+ /usr/include/c++/9/bits/algorithmfwd.h \
+ /usr/include/c++/9/initializer_list /usr/include/c++/9/bits/stl_heap.h \
+ /usr/include/c++/9/bits/stl_tempbuf.h \
+ /usr/include/c++/9/bits/stl_construct.h \
+ /usr/include/c++/9/ext/alloc_traits.h \
+ /usr/include/c++/9/bits/alloc_traits.h \
+ /usr/include/c++/9/bits/uniform_int_dist.h \
+ /usr/include/c++/9/bits/range_access.h \
+ /usr/include/c++/9/bits/basic_string.h /usr/include/c++/9/string_view \
+ /usr/include/c++/9/bits/functional_hash.h \
+ /usr/include/c++/9/bits/string_view.tcc \
+ /usr/include/c++/9/ext/string_conversions.h /usr/include/c++/9/cstdio \
+ /usr/include/c++/9/cerrno /usr/include/c++/9/bits/basic_string.tcc \
+ /usr/include/c++/9/bits/locale_classes.tcc \
+ /usr/include/c++/9/system_error \
+ /usr/include/x86_64-linux-gnu/c++/9/bits/error_constants.h \
+ /usr/include/c++/9/stdexcept /usr/include/c++/9/streambuf \
+ /usr/include/c++/9/bits/streambuf.tcc \
+ /usr/include/c++/9/bits/basic_ios.h \
+ /usr/include/c++/9/bits/locale_facets.h /usr/include/c++/9/cwctype \
+ /usr/include/wctype.h /usr/include/x86_64-linux-gnu/bits/wctype-wchar.h \
+ /usr/include/x86_64-linux-gnu/c++/9/bits/ctype_base.h \
+ /usr/include/c++/9/bits/streambuf_iterator.h \
+ /usr/include/x86_64-linux-gnu/c++/9/bits/ctype_inline.h \
+ /usr/include/c++/9/bits/locale_facets.tcc \
+ /usr/include/c++/9/bits/basic_ios.tcc /usr/include/c++/9/ostream \
+ /usr/include/c++/9/bits/ostream.tcc /usr/include/c++/9/bits/istream.tcc \
+ /usr/include/c++/9/bits/sstream.tcc /home/alrobles/powerlaw/src/Zeta.h
